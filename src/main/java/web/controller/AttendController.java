614858b3a9daa9870ced2b61daea86e29f4a2b59
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-17 02:09:06 +0900

    12_최규호_출석하기,출석조회_API 테스트 완료

--- src/main/java/web/controller/AttendController.java
+++ src/main/java/web/controller/AttendController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import web.model.dto.user.AttendDto;
 import web.service.AttendService;
+import web.util.AuthUtil;
 
 import java.util.List;
 
@@ -17,24 +18,24 @@ import java.util.List;
 public class AttendController {
 
     private final AttendService attendService;
+    private final AuthUtil authUtil;
 
     // [AT-1] 출석하기 attend()
     @PostMapping("/attend")
     public ResponseEntity<?> attend(@RequestBody AttendDto attendDto, HttpServletRequest request){
         try {
-            // 세션 정보 가져오기
-            HttpSession session = request.getSession();
+            // 세션 또는 토큰 가져오기
+            Integer userNo = authUtil.getUserNo(request);
             // 로그인 성공한 사용자 번호 확인
-            if (session == null || session.getAttribute("userNo") == null) {
-                return ResponseEntity.status(400).body(null);
+            if (userNo == null ) {
+                System.out.println("인증 실패: userNo를 가져올 수 없음");
+                return ResponseEntity.status(401).body(null);
             }
-            // 모든 자료를 저장하기 위해 Object 타입으로 세션 저장
-            Object obj = session.getAttribute("userNo");
-            if (obj == null) return ResponseEntity.status(400).body(null);
-            int userNo = (int) obj;
+            // 로그인된 사용자번호 꺼내기 = 수정하는 사용자의 번호
+            System.out.println("현재 수정할 사용자의 번호 : "+userNo);
 
+            // dto 담아주기
             attendDto.setUserNo(userNo);
-
             int result = attendService.attend(attendDto);
             System.out.println("출석체크 성공시 1 반환 : "+result);
             return ResponseEntity.status(200).body(result);
@@ -48,16 +49,13 @@ public class AttendController {
     // [AT-2] 출석 조회 getAttend()
     @GetMapping("/attend")
     public ResponseEntity<List<AttendDto>> getAttend(HttpServletRequest request){
-        // 세션 정보 가져오기
-        HttpSession session = request.getSession();
+        // 세션 또는 토큰 가져오기
+        Integer userNo = authUtil.getUserNo(request);
         // 로그인 성공한 사용자 번호 확인
-        if ( session == null || session.getAttribute("userNo")==null){
-            return ResponseEntity.status(400).body(null);
+        if ( userNo ==null){
+            return ResponseEntity.status(401).body(null);
         }
-        // 모든 자료를 저장하기 위해 Object 타입으로 세션 저장
-        Object obj = session.getAttribute("userNo");
-        if ( obj == null) return ResponseEntity.status(400).body(null);
-        int userNo = (int)obj;
+        // List 담아주기
         List<AttendDto> result = attendService.getAttend(userNo);
         return ResponseEntity.status(200).body(result);
     }
