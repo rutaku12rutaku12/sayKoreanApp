f3f7c2a166208a5144e5bae444471f96fc781a55
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-24 12:38:50 +0900

    22_최규호_회원가입 비밀번호 중복 검사, 비밀번호 관련 DTO 8자리 이상 @Pattern 추가

--- src/main/java/web/model/dto/user/LoginDto.java
+++ src/main/java/web/model/dto/user/LoginDto.java
@@ -2,6 +2,7 @@ package web.model.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class LoginDto {
     @NotBlank
     private String email;
     @NotBlank
+    @Pattern(regexp = "^[\\S]{8,}$" , message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String password;
     private int userNo;
     private int userState =1;
